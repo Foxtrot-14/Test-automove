57b492cfd808ad1b3a964988f69a6d5e1ee1c966
Foxtrot-14 <noamanahmedai@gmail.com>
2023-12-13 03:39:45 +0530

    html-files-updated

--- NPM/Import/index.js
+++ NPM/Import/index.js
@@ -2,3 +2,4 @@ import cytoscape from "cytoscape";
 import automove from "cytoscape-automove-foxtrot-test";
 
 cytoscape.use(automove);
+
--- NPM/Require/index.js
+++ NPM/Require/index.js
@@ -2,3 +2,4 @@ const cytoscape = require("cytoscape");
 const automove = require("cytoscape-automove-foxtrot-test");
 
 cytoscape.use(automove);
+console.log(automove);
