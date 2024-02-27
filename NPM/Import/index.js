e5d54e743e05e9e281917c64beda1ab56d8ff747
Foxtrot-14 <noamanahmedai@gmail.com>
2024-02-27 23:20:53 +0530

    Assertion-sytax-fixed

--- NPM/Import/index.js
+++ NPM/Import/index.js
@@ -1,10 +1,13 @@
+import assert from "assert";
 import cytoscape from "cytoscape";
 import automove from "cytoscape-automove-foxtrot-test";
 
 cytoscape.use(automove);
 const cy = cytoscape();
-if (Object.getPrototypeOf(cy).automove != null) {
-  console.log("Extension Added");
-} else {
-  console.assert(Object.getPrototypeOf(cy).automove != null, "Null");
-}
+assert(Object.getPrototypeOf(cy).automove != null);
+console.log("Assertion Passed")
+// if (Object.getPrototypeOf(cy).automove != null) {
+//   console.log("Extension Added");
+// } else {
+//   console.assert(Object.getPrototypeOf(cy).automove != null, "Null");
+// }
--- NPM/Require/index.js
+++ NPM/Require/index.js
@@ -1,9 +1,12 @@
 const cytoscape = require("cytoscape");
 const automove = require("cytoscape-automove-foxtrot-test");
+const assert = require("assert");
 cytoscape.use(automove);
 const cy = cytoscape();
-if (Object.getPrototypeOf(cy).automove != null) {
-  console.log("Extension Added");
-} else {
-  console.assert(Object.getPrototypeOf(cy).automove != null, "Null");
-}
+assert(Object.getPrototypeOf(cy).automove != null);
+console.log("Assertion Passed");
+// if (Object.getPrototypeOf(cy).automove != null) {
+//   console.log("Extension Added");
+// } else {
+//   console.assert(Object.getPrototypeOf(cy).automove != null, "Null");
+// }
